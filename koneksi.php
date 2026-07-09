0ef4ee6b0d22b343721ab1648da0c3acaa11a152
alin-dotcom13 <alinacantika9@gmail.com>
2026-07-10 01:24:19 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,6 +4,11 @@ $port = getenv('MYSQLPORT');
 $db   = getenv('MYSQLDATABASE');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
-$pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi Gagal: " . $e->getMessage());
+}
 ?>
