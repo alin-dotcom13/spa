ff6d88e4a9f6beb0b30d7107226e7c537be9d336
Alin <alinnaww9@gmail.com>
2026-07-09 21:30:03 +0700

    update  sekali lagi

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,14 @@
 <?php
+
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
 
-
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
+$conn = @mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+    echo "Koneksi database bermasalah: " . mysqli_connect_error();
 }
 ?>
